0bdfb229196b0a037b02a79d9a595f109007b1b6
ajberchek <ajberchek@gmail.com>
2017-06-21 22:34:58 -0700

    Made class to manage the cabinet layout, shelves will be added to a row in a location given by (lastShelfXPos + sequence[i]) mod width until the numberOfShelvesPerRow is reached, then it will move on

---
+++ CabinetPlanner/CabinetConstraints.py
@@ -0,0 +1,38 @@
+import sys
+class CabinetConstraints:
+    def __init__(self, width, height, rowHeight, numShelvesPerRow):
+        self.width = width
+        self.height = height
+        self.rowHeight = rowHeight
+        self.numShelvesPerRow = numShelvesPerRow
+        self.numRows = int(height/rowHeight)
+        self.shelfLayout = [["_" for x in range(self.width)] for y in range(self.numRows)]
+
+    def getRequiredSequenceSize(self):
+        return self.numRows*self.numShelvesPerRow + 100
+
+    def clearLayout(self):
+        self.shelfLayout = [["_" for x in range(self.width)] for y in range(self.numRows)]
+
+    def layoutShelves(self,sequence):
+        print(len(sequence))
+        lastShelfPos = 0
+        for i in range(self.numRows):
+            for j in range(self.numShelvesPerRow):
+                sequenceIndex = i*self.numShelvesPerRow + j
+
+                lastShelfPos = (lastShelfPos + sequence[sequenceIndex]) % self.width
+                self.shelfLayout[i][lastShelfPos] = "|"
+
+    def __str__(self):
+        toRet = " "
+        for i in range(self.width):
+            toRet += "_"
+        toRet += " \n"
+        for i in range(self.numRows):
+            toRet += "|"
+            for j in range(self.width):
+                toRet += self.shelfLayout[i][j]
+            toRet += "|\n"
+        return toRet
+
