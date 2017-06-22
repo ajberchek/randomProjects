a19f657cf5dc4e78bd28180a5286d55cd62a13c3
ajberchek <ajberchek@gmail.com>
2017-06-21 21:16:36 -0700

    Created a fibonacci sequence generator that runs in O(n) although, it wont need to be optimized for the small n we will be using for this project

---
+++ CabinetPlanner/fibonacciGenerator.py
@@ -0,0 +1,15 @@
+from sequenceGenerator import sequenceGenerator
+class fibonacciGenerator(sequenceGenerator):
+    def __init__(self,numElems):
+        super.__init__(numElems)
+        self.fibSequence = []
+
+    def getSequence(self):
+        if(len(self.fibSequence) != 0):
+            return self.fibSequence
+
+        self.fibSequence.extend((0,1))
+        for i in range(len(self.fibSequence),self.numElems):
+            self.fibSequence.append(self.fibSequence[i-2]+self.fibSequence[i-1])
+            
+        return self.fibSequence
--- CabinetPlanner/sequenceGenerator.py
+++ CabinetPlanner/sequenceGenerator.py
@@ -2,7 +2,7 @@ class sequenceGenerator:
     def __init__(self,numElems):
         self.numElems = numElems
 
-    def generate(self):
+    def getSequence(self):
         return []
 
     def setNumElems(self,numElems):
