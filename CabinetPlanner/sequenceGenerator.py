5ccfba3d64d567ce9ab808c74e001cb48b56805d
ajberchek <ajberchek@gmail.com>
2017-06-21 21:08:18 -0700

    Created sequence generator base class

---
+++ CabinetPlanner/sequenceGenerator.py
@@ -0,0 +1,12 @@
+class sequenceGenerator:
+    def __init__(self,numElems):
+        self.numElems = numElems
+
+    def generate(self):
+        return []
+
+    def setNumElems(self,numElems):
+        self.numElems = numElems
+
+    def getNumElems(self):
+        return self.numElems
